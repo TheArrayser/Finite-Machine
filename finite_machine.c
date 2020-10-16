a5c9473f219688de2c4da028aa67123904d34cd3
TheArrayser <68914060+TheArrayser@users.noreply.github.com>
2020-10-16 23:01:24 +0200

    Add files via upload
    P.41/2d) - works fine! Everything is described!

---
+++ finite_machine.c
@@ -0,0 +1,129 @@
+//=====Author: The Arrayser=====//
+//This is just a demonstration of the finite machine!
+//This is the main project file (finite_machine.c)
+//There should be no need to change anything from the this file!
+//
+//I CANNOT GIVE WARRANTY, THAT THIS ALWAYS WORKS!
+
+
+#include <stdio.h>
+
+#include "E:\sdl engine test\finite_machine.h"
+#include "E:\sdl engine test\finite_machine_modificators.h"
+
+unsigned int possibleInputLength = 50; //Propably you might want to
+
+void InitializeMembers();
+Node *doesEdgeListContainLetter(char, Edge *);
+
+int main(){
+    InitializeMembers();
+    
+    char input[possibleInputLength];
+    printf("Type in a word to check if it works with the machine! Press [Enter], once you want to check your word!\n");
+    printf("You can at maximum type %u characters\n", possibleInputLength);
+    printf("The alphabet is \"%s\" with a length of %i\n", alphabet, sizeof(alphabet)-1);
+    printf("Your word: ");
+    scanf("%s", input);
+    printf("The input = %s\n", input);
+    for(int i= 0; i<possibleInputLength; i++){
+        if(input[i]!='\0'){
+            for(int a = 0; a < sizeof(alphabet-1); a++){
+                if(input[i]==(alphabet[a])){
+                    goto cc;
+                }else{
+                    continue;
+                }
+                break;
+            }
+        }else{
+            break;
+        }
+        goto end1;
+        break;
+        cc:
+        continue;
+    }
+    printf("your word passed stage 1\n");
+    
+    
+    Node *tempnode = Start.following;
+    Edge *tempedges = Start.following->edges;
+
+    for(int k = 0; k < possibleInputLength; k++){
+        char charForComparison = input[k];
+        if((charForComparison=='\0')||(k==possibleInputLength-1)){
+            if(tempnode->isPossibleEnd){
+                goto finish;
+            }else{
+                goto Error;
+            }
+
+        }
+        Node *moretempnode =doesEdgeListContainLetter(charForComparison, tempedges);
+        if(moretempnode!=NULL){
+            tempnode = moretempnode;
+            tempedges = tempnode->edges;
+            continue;
+        }else{
+            //printf("yeah, still not compatible!\n");
+            return 0;
+        }
+        break;
+    }
+
+    Error:
+    printf("Unfortunatly, the word did NOT pass the finite machine!\n");
+    return 0;
+
+    finish:
+    printf("Concrats! The word passed the finite machine!\n");
+    return 0;
+
+    end1:
+    printf("The word you chose is not compatible with the machine\n");
+}
+
+boolean doesEdgeContainLetter(char, Edge *);
+
+Node *doesEdgeListContainLetter(char cmpchar ,Edge *testEdgelist){
+        Edge *tempedge = testEdgelist;
+        while(tempedge->next != NULL){
+            if(doesEdgeContainLetter(cmpchar, tempedge)){
+                return tempedge->following;
+            }else{
+                tempedge = tempedge->next;
+            }
+        }
+            
+        if(doesEdgeContainLetter(cmpchar, tempedge)){
+            return tempedge->following;
+        }else{
+            tempedge = tempedge->next;
+        }
+}
+
+boolean doesEdgeContainLetter(char cmpchar, Edge *testEdge){
+        Lang *templang = testEdge->partial_alphabet;
+
+        while((templang->next) != NULL){
+            if(templang->symbol=='\0'){
+                printf("Error #2: unexpected null found!\n");
+            }
+            if(templang->symbol==cmpchar){
+                return true;
+            }
+            templang = templang->next;
+        }
+        if(templang->symbol=='\0'){
+                printf("Error #3: unexpected null found!\n");
+            }
+        if(templang->symbol==cmpchar){
+            return true;
+        }
+    return false;
+}
+
+void InitializeMembers(){
+    Initialization();
+}
---
+++ finite_machine_modificators.h
@@ -0,0 +1,43 @@
+//=====Author: The Arrayser=====//
+//This is just a demonstration of the finite machine!
+//Use this file (finite_machine_modificators.h) to describe the machine!
+//
+//Demonstration of: p.41/2d)
+//
+//There should be no need to change anything from the other files!
+//
+//I CANNOT GIVE WARRANTY, THAT THIS ALWAYS WORKS!
+
+#ifndef FINITE_MACHINE
+#include "E:\sdl engine test\finite_machine.h"
+#endif
+
+
+char alphabet[] = {"ab"};
+
+Node Z1;
+Node Z2;
+Node Z3;
+
+Lang pos1 = {'a', NULL};
+Lang pos2 = {'b', NULL};
+
+Edge Z1b = {&Z2,NULL, &pos2, true};
+Edge Z1a = {&Z1,&Z1b ,&pos1, false};
+
+Edge Z2b ={&Z3,NULL, &pos2, true};
+Edge Z2a ={&Z2,&Z2b, &pos1, false};
+
+Edge Z3b ={&Z2,NULL, &pos2, true};
+Edge Z3a ={&Z3,&Z3b, &pos1, false};
+
+
+void Initialization(){
+    Start.following = &Z1;
+
+    Z3 = (Node){"Z3",&Z3a ,true};
+    Z2= (Node) {"Z2", &Z2a, false};
+    Z1 = (Node){"Z1", &Z1a, false};
+
+    
+}
